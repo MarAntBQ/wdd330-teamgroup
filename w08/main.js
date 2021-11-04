e3bfc8439f043ead0c76810b4b239182aa6a4c8e
ntduncan <nateduncan0@.com>
2021-11-03 20:00:35 -0600

    pokestuff

---
+++ w08/main.js
@@ -0,0 +1,49 @@
+const pokemonNames = ["pikachu", "eevee", "charizard", "meowth", "dialga"]
+let pokemonData = []
+
+
+pokemonNames.forEach(pokemon => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    .then(response => response.json())
+    .then(body =>  {
+        console.log(body)
+
+        const obj = {
+            name: body.name,
+            id: body.id,
+            weight: body.weight,
+            type: body.types[0].type.name
+        }
+        pokemonData.push(obj);
+        console.log(obj)
+        return obj
+    })
+    .then((obj) => {
+        const pokemon = document.createElement('div');
+        pokemon.classList.add("pokemon");
+
+        const name = document.createElement("h3")
+        name.textContent = "name: " + obj.name;
+        name.classList.add("pokemon-name")
+
+        const id = document.createElement("p")
+        id.classList.add("pokemon-id")
+        id.textContent = "id: " + obj.id;
+
+        const weight = document.createElement("p")
+        weight.classList.add("pokemon-weight")
+        weight.textContent = "weight: " + obj.weight;
+        
+        const type = document.createElement("p")
+        type.classList.add("pokemon-type")
+        type.textContent = "type: " + obj.type;
+
+        pokemon.appendChild(name)
+        pokemon.appendChild(id)
+        pokemon.appendChild(weight)
+        pokemon.appendChild(type)
+
+        document.querySelector(".pokemans").appendChild(pokemon);
+    })
+    .catch(err => console.log(err))
+}) 
